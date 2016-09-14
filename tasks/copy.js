dad19ce763dcecc1f27bf594044831dffb1325cf
Wu_TiHou <wutiehao@126.com>
2016-09-14 17:58:03 +0800

    ....

--- tasks/copy.js
+++ tasks/copy.js
@@ -5,5 +5,7 @@ var gulp = require('gulp');
 gulp.task('copy', function(){
     gulp.src('app/js/**/*.js')
         .pipe(gulp.dest('app/dist/js/'));
+    gulp.src('app/css/**/*.css')
+        .pipe(gulp.dest('app/dist/css/'));
     console.log("文件任务完成！");
 });
